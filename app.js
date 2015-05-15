bdb66daa5912d1151d33bea702d01c5e74cd13cd
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-05-14 22:46:48 -0400

    stf

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ process.env.PWD = process.cwd();
 
 app.use(express.static(process.env.PWD + '/public'));
 var port = process.env.PORT || 8000
-app.listen(8000);
+app.listen(port);
 console.log('Listening on port 8000');
