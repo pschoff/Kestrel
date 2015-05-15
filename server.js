38ae5a3666e5bee0beac4d97e16f0cf96e3a2f55
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-05-14 22:11:00 -0400

    static server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+
 process.env.PWD = process.cwd();
 
-app.use(express.static(path.join(process.env.PWD, 'public'))); 
 
+app.use(express.static(process.env.PWD + '/public'));
 app.listen(3000);
 console.log('Listening on port 3000');
