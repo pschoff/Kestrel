0163bb0a2cf5102ff1005d9b1bea5fbb3088b75d
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-05-14 21:40:27 -0400

    gonna work

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+process.env.PWD = process.cwd();
 
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(process.env.PWD, 'public'))); 
 
 app.listen(3000);
-console.log('Listening on port 80');
+console.log('Listening on port 3000');
