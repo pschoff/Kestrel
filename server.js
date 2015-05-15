f5ade33223057a3ea4266e4e7765fff5c347e481
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-05-14 21:02:40 -0400

    express server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+var staticPath = path.resolve(__dirname);
+app.use(express.static(staticPath));
+
+app.listen(3000, function() {
+  console.log('listening');
+});
